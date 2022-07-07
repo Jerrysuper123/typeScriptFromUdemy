265ba6a0a2717f67a41db2e1cb4418a76e036e49
Jerry Chen <jerryq.chen@gmail.com>
2022-07-07 13:58:39 +0800

    added basics primitive types

---
+++ basics.ts
@@ -0,0 +1,18 @@
+//primitve, number, string, boolean
+//complex - arrays and objects
+//function type and parameter
+
+//primitive
+let a: number = 3;
+a = 3;
+
+let b: string;
+b = "hahah";
+
+let isTrue: boolean = true;
+isTrue = false;
+
+let isFun: null;
+//null type is not very useful
+// isFun = "haha";
+
